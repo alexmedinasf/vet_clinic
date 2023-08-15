956329acd4fb4d6192c14f0e3bf97703012d675d
Alexandre <alexsales001@hotmail.com>
2023-08-15 10:50:32 -0300

    update data.sql with new entries

--- data.sql
+++ data.sql
@@ -14,3 +14,32 @@ INSERT INTO public.animals(
 	name, date_of_birth, escape_attempts, neutered, weight_kg)
 	VALUES ('Devimon', '2017-05-12', 5, '1', '11');
     
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Charmander', '2020-02-08', '0', '0', '11');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Plantmon', '2021-11-15', '2', '1', '5.7');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Squirtle', '1993-04-02', '3', '0', '12.13');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Angemon', '2005-06-12', '1', '1', '45');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Boarmon', '2005-06-07', '7', '1', '20.4');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Blossom', '1998-10-13', '3', '1', '17');
+
+	INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Ditto', '2022-05-14', '4', '1', '22');
+
+	
